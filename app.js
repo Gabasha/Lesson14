3e72c3a79fd54d14253c6a9e145e90a6a8223743
Gabasha <gabashidze05@gmail.com>
2024-04-04 22:57:34 +0400

    !

---
+++ app.js
@@ -0,0 +1,24 @@
+let numbers=[1936,1939,1940,1942,1945]
+let sum=numbers[0]+numbers[1]+numbers[2]+numbers[3]+numbers[4];
+let shsualo=sum/numbers.length
+console.log(sum)
+console.log(shsualo)
+
+// ეს მეორე დავალება ვერ გავიგე <3
+let masivi=[
+    {name:"giorgi", age:18},
+    {name:"ani", age:19},
+    {name:"luka", age:18},
+    {name:"nikolozi", age:17},
+    {name:"saba", age:18}
+]
+console.log(masivi)
+
+const Personaldata={
+    firstName:"Giorgi",
+    lastName:"Abashidze",
+    address:["D.Abashize N5", "K.Abashidze N2"],
+    age:18,
+    phoneNumbers:599999999
+}
+console.log(`My name is ${Personaldata["firstName"]}, My age is ${Personaldata["age"]}, My address is ${Personaldata["address"]}` )
